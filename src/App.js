079886905b7f084073914968046ca52ce676c9e1
LuaanNguyen <luanthiennguyen2003@gmail.com>
2024-07-24 15:48:00 -0700

    Removed Files

--- src/App.js
+++ src/App.js
@@ -1,37 +1,22 @@
-
-import './App.css';
-import './components/otherStuff.css'
-import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
-import Cast from './components/cast.jsx';
-import Home from './components/home.jsx';
-import ReleaseInfo from './components/ReleaseInfo.jsx';
-import Navbar from './components/Navbar.js';
-import Videos from './components/videos.jsx';
-
+import "./App.css";
+import "./components/otherStuff.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Cast from "./components/cast.jsx";
+import Home from "./components/home.jsx";
+import ReleaseInfo from "./components/ReleaseInfo.jsx";
+import Navbar from "./components/Navbar.js";
+import Videos from "./components/videos.jsx";
 
 function App() {
   return (
     <>
-    <Navbar />
+      <Navbar />
       <BrowserRouter>
         <Routes>
-          <Route
-            exact
-            path='/'
-            element={<Home/>}
-          />
-          <Route
-            path = "/cast"
-            element={<Cast/>}
-          />
-          <Route
-            path = "/release-info"
-            element={<ReleaseInfo/>}
-          />
-          <Route
-            path = "/videos"
-            element={<Videos/>}
-          />
+          <Route exact path="/" element={<Home />} />
+          <Route path="/cast" element={<Cast />} />
+          <Route path="/release-info" element={<ReleaseInfo />} />
+          <Route path="/videos" element={<Videos />} />
         </Routes>
       </BrowserRouter>
     </>
