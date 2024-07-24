e9c98cd98007886d12f75ec361805f2e4fa8aa10
LuaanNguyen <luanthiennguyen2003@gmail.com>
2024-07-24 16:23:20 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import "./components/otherStuff.css";
-import { Routes, Route, HashRouter } from "react-router-dom";
+import { Routes, Route, HashRouter, BrowserRouter } from "react-router-dom";
 import Cast from "./components/cast.jsx";
 import Home from "./components/home.jsx";
 import ReleaseInfo from "./components/ReleaseInfo.jsx";
@@ -10,7 +10,7 @@ import Videos from "./components/videos.jsx";
 function App() {
   return (
     <>
-      <HashRouter>
+      <BrowserRouter>
         <Navbar />
         <Routes>
           <Route exact path="/" element={<Home />} />
@@ -18,7 +18,7 @@ function App() {
           <Route path="/release-info" element={<ReleaseInfo />} />
           <Route path="/videos" element={<Videos />} />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </>
   );
 }
